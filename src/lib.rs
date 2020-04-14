266d5f7be6b8c0656962061305cc5b3b0a902c3f
James W. Fawcett <jfawcett@twcny.rr.com>
2020-04-13 20:30:16 -0400

    added doc comments to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,46 @@
 //                                                         //
 // Jim Fawcett, https://JimFawcett.github.io, 12 Apr 2020  //
 /////////////////////////////////////////////////////////////
-
+/*
+   DirNav<App> is a directory navigator that  uses the generic
+   parameter App to define how files and directories are
+   handled.
+*/
 use std::io;
 use std::fs::{self, DirEntry};
 #[allow(unused_imports)]
 use std::path::{Path, PathBuf};
-//use std::ffi::OsStr;
 
+/// trait required of the App generic parameter type
 pub trait DirEvent {
   fn do_dir(&mut self, d:&String);
   fn do_file(&mut self, f:&String);
 }
-struct Appl;
-impl DirEvent for Appl {
-    fn do_dir(&mut self, d:&String) {
-        print!("\n  {:?}", d);
-    }
-    fn do_file(&mut self, f:&String) {
-        print!("\n    {:?}", f);
-    }    
-}
+//---------------------------------------
+// Sample implementation of DirNav param
+// -------------------------------------- 
+// struct Appl;
+// impl DirEvent for Appl {
+//     fn do_dir(&mut self, d:&String) {
+//         print!("\n  {:?}", d);
+//     }
+//     fn do_file(&mut self, f:&String) {
+//         print!("\n    {:?}", f);
+//     }    
+// }
 
 type Patterns = Vec::<std::ffi::OsString>;
 
+/// Directory Navigator Structure
 #[allow(dead_code)]
 pub struct DirNav<App:DirEvent> { 
+    /// file exts to look for
     pats:Patterns,
+    /// instance of App : DirEvent
     app:App,
+    /// number of files processed
     num_fun : usize,
+    /// number of dirs processed
     num_dir : usize,
 }
 impl<App:DirEvent> DirNav<App> {
@@ -42,22 +54,26 @@ impl<App:DirEvent> DirNav<App> {
             num_dir: 0,
         }
     }
-    pub fn get_app(&mut self) -> &App {
-        &self.app
-    }
+    /// return reference to App to get results
+    pub fn get_app(&mut self) -> &App { &self.app }
+    /// return number of dirs processed
     pub fn get_dirs(&self) -> usize { self.num_dir }
+    /// return number of files processed
     pub fn get_funs(&self) -> usize { self.num_fun }
 
-    /*-- takes either String or &str --*/
+    /// add extention to search for
+    /// takes either String or &str
     pub fn add_pat<S:Into<String>>(&mut self, p:S) -> &mut DirNav<App> {
         let mut t = std::ffi::OsString::new();
         t.push(p.into());
         self.pats.push(t);
         self
     }
+    /// remove all patterns from store
     pub fn clear_pat(&mut self) {
         self.pats.clear();
     }
+    /// Depth First Search for file extentions starting at path dir
     pub fn visit(&mut self, dir: &Path) 
              -> io::Result<()> where App:DirEvent {
         self.num_dir += 1;
@@ -86,12 +102,12 @@ impl<App:DirEvent> DirNav<App> {
         }
         Ok(())
     }
-
+    /// replace Windows directory seperator with Linux seperator
     pub fn replace_sep(&self, path:&Path) -> String {
         let rtn = path.to_string_lossy();
         rtn.replace("\\", "/")
     }
-
+    /// does store contain d.path().extension() ?
     pub fn in_patterns(&self, d:&DirEntry) -> bool {
         let p = d.path();
         let ext = p.extension();
@@ -106,6 +122,10 @@ impl<App:DirEvent> DirNav<App> {
 
 #[cfg(test)]
 mod tests {
+    // test_setup() should run first. To ensure that:
+    //   use cargo -- --test-threads=1
+    // to see console output:
+    //   use cargo test -- --show-output --test-threads=1
     use super::*;
     #[test]
     fn test_setup() {
@@ -119,15 +139,15 @@ mod tests {
     }
     #[test]
     fn test_walk() {
-        // let rslt_store = Vec::<String>::new();
-        // let l = |f:String|  { rslt_store.push(f); };
+        /// ApplTest - generic parameter for testing DirNav<ApplTest>
+        /// rslt_store holds results of file search
         #[derive(Debug)]
         struct ApplTest {
             rslt_store: Vec<String>,
         }
         impl DirEvent for ApplTest {
-            fn do_dir(&mut self, d:&String) {
-                print!("\n  {:?}", d);
+            fn do_dir(&mut self, _d:&String) {
+                //print!("\n  {:?}", d);
             }
             fn do_file(&mut self, f:&String) {
                 //print!("\n    {:?}", f);
@@ -147,6 +167,7 @@ mod tests {
         */
         print!("\n  {:?}",rl);
 
+        // test for found files
         let l = |s:&str| -> String { s.to_string() };
 
         assert!(rl.contains(&l("test_file.rs")));
