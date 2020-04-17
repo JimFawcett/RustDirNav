8399987e52b22842e9d2ffd105a3f75013532823
James W. Fawcett <jfawcett@twcny.rr.com>
2020-04-17 10:21:54 -0400

    added comment about testing error return

--- target/debug/deps/rust_dir_nav-1cb9e0028534d47d.d
+++
@@ -1,7 +0,0 @@
-C:\github\JimFawcett\RustDirNav\target\debug\deps\rust_dir_nav-1cb9e0028534d47d.rmeta: src\lib.rs
-
-C:\github\JimFawcett\RustDirNav\target\debug\deps\librust_dir_nav-1cb9e0028534d47d.rlib: src\lib.rs
-
-C:\github\JimFawcett\RustDirNav\target\debug\deps\rust_dir_nav-1cb9e0028534d47d.d: src\lib.rs
-
-src\lib.rs:
--- target/debug/rust_dir_nav-af46debae34e1ef3.d
+++
@@ -1,1 +0,0 @@
-C:\github\JimFawcett\RustDirNav\target\debug\rust_dir_nav-af46debae34e1ef3.exe: C:\github\JimFawcett\RustDirNav\src\lib.rs
